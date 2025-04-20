fba5f10cfa02d44f8456f843c5164949e3db27ef
Daniel-Frederick <frederickd5@udayton.edu>
2025-04-20 14:30:34 -0400

    goblin die when falling

--- Slingshot/Assets/Scripts/GoblinController.cs
+++ Slingshot/Assets/Scripts/GoblinController.cs
@@ -2,11 +2,15 @@ using UnityEngine;
 
 public class GoblinController : MonoBehaviour
 {
+    // private float velocityThreshold = .8;
     private void OnCollisionEnter2D(Collision2D collision)
     {
         // If a box hits an enemy at high speed
-        if (collision.contacts[0].normal.y < -.5)
+        if (collision.gameObject.CompareTag("Box") && collision.contacts[0].normal.y < -.5)
         {
+            Destroy(gameObject);
+        }
+        if (collision.gameObject.CompareTag("Road") && collision.relativeVelocity.y > .8) {
             Destroy(gameObject);
         }
     }
